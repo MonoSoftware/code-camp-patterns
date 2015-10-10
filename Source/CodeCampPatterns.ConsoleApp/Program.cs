dbd75bc63cd869a2568b9b029b976e64eec702e9
Darjan Bogdan <darjan@mono.hr>
2015-10-10 16:37:01 +0200

    Project setup

---
+++ Source/CodeCampPatterns.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿#define Structural
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCampPatterns.Patterns.Implemented;
+using CodeCampPatterns.Patterns.Structural;
+
+namespace CodeCampPatterns.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if Structural
+            //Example with the structural code
+            
+
+#else
+            //Example with the implemented pattern on a real-world example
+            
+
+#endif
+        }
+    }
+}
