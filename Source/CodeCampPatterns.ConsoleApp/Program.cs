44c8e1c714593e7456f6ca62e65a33ad8e88c4d7
Darjan Bogdan <darjan@mono.hr>
2015-10-10 16:41:37 +0200

    Singleton pattern

--- Source/CodeCampPatterns.ConsoleApp/Program.cs
+++ Source/CodeCampPatterns.ConsoleApp/Program.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CodeCampPatterns.Patterns.Implemented;
 using CodeCampPatterns.Patterns.Structural;
 
 namespace CodeCampPatterns.ConsoleApp
@@ -16,8 +15,22 @@ namespace CodeCampPatterns.ConsoleApp
         {
 #if Structural
             //Example with the structural code
-            
 
+            //Uncomment to see compile time inaccessibility due to protection level of the constructor
+            //Singleton objError = new Singleton();
+
+            Console.WriteLine("Get instance of singleton object for the first time:");
+            Singleton objFirst = Singleton.GetInstance();
+            Console.WriteLine();
+
+            Console.WriteLine("Get instance of singleton object again:");
+            Singleton objSecond = Singleton.GetInstance();
+            Console.WriteLine();
+
+            Console.WriteLine("Check whether \"objFirst\" and \"objSecond\" are trully the same:");
+            Console.WriteLine("Check against reference equality: {0}", Object.ReferenceEquals(objFirst, objSecond));
+
+            Console.ReadLine();
 #else
             //Example with the implemented pattern on a real-world example
             
---
+++ Source/CodeCampPatterns.Patterns/Structural/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampPatterns.Patterns.Structural
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+            Console.WriteLine("3. Inside private constructor");
+        }
+
+        public static Singleton GetInstance()
+        {
+            Console.WriteLine("1. Check whether the instance is null.");
+            if (instance == null)
+            {
+                Console.WriteLine("2. Instance object is null at the moment, create a Singleton object and assign it to the instance variable.");
+                instance = new Singleton();
+                Console.WriteLine("4. Object is created and assigned to the instance variable.");
+            }
+            Console.WriteLine("5. Object is assigned to the instance, return it!");
+            return instance;
+        }
+    }
+}
