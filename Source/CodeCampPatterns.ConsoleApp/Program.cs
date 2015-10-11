13a50e0ff131409d70997368c66f2a6712f16497
Darjan Bogdan <darjan@mono.hr>
2015-10-11 14:53:10 +0200

    Prototype pattern

--- Source/CodeCampPatterns.ConsoleApp/Program.cs
+++ Source/CodeCampPatterns.ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿#define Structural
+﻿//#define Structural
 
 using System;
 using System.Collections.Generic;
@@ -16,12 +16,24 @@ namespace CodeCampPatterns.ConsoleApp
         {
 #if Structural
             //Example with the structural code
-            
+            IPrototype p1 = new Prototype();
+            IPrototype p2 = p1.Clone();
 
+            Console.WriteLine(String.Format("Checke whether objects \"p1\" and \"p2\" are equal: {0}", Object.ReferenceEquals(p1, p2)));
+            Console.ReadLine();
 #else
             //Example with the implemented pattern on a real-world example
-            
+            Console.WriteLine("First car:");
+            Console.WriteLine("Creation started: {0}", DateTime.UtcNow);
+            ICar mercedesFirst = new Mercedes("A Class", new { CarParts = "Everything a car needs to be assembled properly." });
+            Console.WriteLine("Creation ended: {0}", DateTime.UtcNow);
 
+            Console.WriteLine("Second car:");
+            Console.WriteLine("Creation started: {0}", DateTime.UtcNow);
+            ICar mercedesSecond = mercedesFirst.Clone();
+            Console.WriteLine("Creation ended: {0}", DateTime.UtcNow);
+
+            Console.ReadLine();
 #endif
         }
     }
---
+++ Source/CodeCampPatterns.Patterns/Implemented/Mercedes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampPatterns.Patterns.Implemented
+{
+    public class Mercedes : ICar
+    {
+        public string Name { get; set; }
+
+        public Mercedes(string name, object carParts)
+        {
+            //Assemble car
+            //For the explanation purpose, let's say assembling lasts 20seconds
+            Task.Delay(20000).Wait();
+        }
+
+        public ICar Clone()
+        {
+            return (ICar)this.MemberwiseClone();
+        }
+    }
+}
